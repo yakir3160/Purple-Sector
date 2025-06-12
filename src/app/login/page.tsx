14ca3a90fdf631ab411dd6fff2ac256f70a7f0da
yakir3160 <132565923+yakir3160@users.noreply.github.com>
2025-06-12 10:36:20 +0300

    fix: Update tab button styles to use top border for active state

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,17 +10,17 @@ export default function LoginPage() {
   const [activeTab, setActiveTab] = useState<'login' | 'register'>('login');
 
   return (
-    <div className="min-h-[90vh] flex items-start md:items-center justify-center bg-background px-4 py-10 md:py-0 sm:px-6 lg:px-8 h-fit">
+    <div className="min-h-[90vh] flex items-start  justify-center bg-background px-4 py-10  sm:px-6 lg:px-8 h-fit">
       <ToastContainer position="top-center" autoClose={3000} />
-      <div className="rounded-lg shadow-lg max-w-md w-full bg-white overflow-hidden transition-all duration-300 ease-in-out opacity-100">
+      <div className="shadow-lg max-w-md w-full bg-white overflow-hidden transition-all duration-300 ease-in-out opacity-100">
         {/* Tab Navigation */}
         <div className="flex text-center">
           <button
             onClick={() => setActiveTab('login')}
             className={`flex-1 py-4  text-lg transition-all duration-300 relative ${
               activeTab === 'login'
-                ? 'font-formula-bold border-b-2 border-f1-purple text-black bg-white'
-                : 'font-formula-regular border-b-2 border-transparent text-black bg-gray-100 hover:border-f1-purple/20'
+                ? 'font-formula-bold border-t-2 border-f1-purple text-black bg-white'
+                : 'font-formula-regular border-t-2 border-transparent text-black bg-gray-100 hover:border-f1-purple/20'
             }`}
           >
             Login
@@ -29,8 +29,8 @@ export default function LoginPage() {
             onClick={() => setActiveTab('register')}
             className={`flex-1 py-4 text-lg transition-all duration-300 relative ${
               activeTab === 'register'
-                ? 'font-formula-bold border-b-2 border-f1-purple text-black bg-white'
-                : 'font-formula-regular border-b-2 border-transparent text-black bg-gray-100 hover:border-f1-purple/20'
+                ? 'font-formula-bold border-t-2 border-f1-purple text-black bg-white'
+                : 'font-formula-regular border-t-2 border-transparent text-black bg-gray-100 hover:border-f1-purple/20'
             }`}
           >
             Register
